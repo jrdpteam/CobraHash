3fffb6ac5a8163537bcaa0052dd63065898a1606
JRDP Team <136267216+jrdpteam@users.noreply.github.com>
2024-10-03 22:25:08 +0200

    Update cobra_hash.c

--- cobra_hash.c
+++ cobra_hash.c
@@ -4,73 +4,67 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-uint64_t H0 = 0x6a09e667f3bcc908;
-uint64_t H1 = 0xbb67ae8584caa73b;
-uint64_t H2 = 0x3c6ef372fe94f82b;
-uint64_t H3 = 0xa54ff53a5f1d36f1;
+uint64_t A0 = 0x6a09e667f3bcc908;
+uint64_t A1 = 0xbb67ae8584caa73b;
+uint64_t A2 = 0x3c6ef372fe94f82b;
+uint64_t A3 = 0xa54ff53a5f1d36f1;
 
-#define BLOCK_SIZE 64
-#define HASH_PREFIX "<BEGIN_COBRA_HASH>"
-#define HASH_SUFFIX "<END_COBRA_HASH>"
+#define B_SIZE 64
+#define C_PREFIX "<BEGIN_COBRA_HASH>"
+#define C_SUFFIX "<END_COBRA_HASH>"
 
-void process_block(uint8_t *block, uint64_t *H0, uint64_t *H1, uint64_t *H2, uint64_t *H3) {
-    for (int i = 0; i < BLOCK_SIZE; i++) {
-        *H0 = (*H0 + block[i]) ^ (*H1);
-        *H1 = (*H1 + block[i]) ^ (*H2);
-        *H2 = (*H2 + block[i]) ^ (*H3);
-        *H3 = (*H3 + block[i]) ^ (*H0);
-
-        *H0 = (*H0 >> 3) | (*H0 << 61);
-        *H1 = (*H1 >> 5) | (*H1 << 59);
-        *H2 = (*H2 >> 7) | (*H2 << 57);
-        *H3 = (*H3 >> 11) | (*H3 << 53);
+void D(uint8_t *E, uint64_t *A0, uint64_t *A1, uint64_t *A2, uint64_t *A3) {
+    for (int F = 0; F < B_SIZE; F++) {
+        *A0 = (*A0 + E[F]) ^ (*A1);
+        *A1 = (*A1 + E[F]) ^ (*A2);
+        *A2 = (*A2 + E[F]) ^ (*A3);
+        *A3 = (*A3 + E[F]) ^ (*A0);
+        *A0 = (*A0 >> 3) | (*A0 << 61);
+        *A1 = (*A1 >> 5) | (*A1 << 59);
+        *A2 = (*A2 >> 7) | (*A2 << 57);
+        *A3 = (*A3 >> 11) | (*A3 << 53);
     }
 }
 
-void extract_32_bytes(FILE *file, unsigned char *outputBuffer) {
-    size_t bytesRead;
-    int index = 0;
+void cobrahash(FILE *H, unsigned char *X, unsigned char *Y) {
+    uint8_t T[B_SIZE];
+    size_t J;
+    uint64_t A0_temp = A0;
+    uint64_t A1_temp = A1;
+    uint64_t A2_temp = A2;
+    uint64_t A3_temp = A3;
+    int K = 0;
+
+    while ((J = fread(T, 1, B_SIZE, H)) > 0) {
+        D(T, &A0_temp, &A1_temp, &A2_temp, &A3_temp);
+    }
+    
+    sprintf((char *)X, "%016llx%016llx%016llx%016llx", A0_temp, A1_temp, A2_temp, A3_temp);
+    
+    rewind(H);
     unsigned char buffer[1];
     int byteCounter = 0;
-
-    while ((bytesRead = fread(buffer, 1, 1, file)) != 0 && index < 32) {
+    while ((J = fread(buffer, 1, 1, H)) != 0 && K < 32) {
         if (byteCounter % 6 == 0) {
-            outputBuffer[index++] = buffer[0];
+            Y[K++] = buffer[0];
         }
         byteCounter++;
     }
-
-    while (index < 32) {
-        outputBuffer[index++] = 0x00;
+    while (K < 32) {
+        Y[K++] = 0x00;
     }
 }
 
-void print_hex(unsigned char *hash, int length) {
-    for (int i = 0; i < length; i++) {
-        printf("%02x", hash[i]);
+void N(unsigned char *O, int P) {
+    for (int Q = 0; Q < P; Q++) {
+        printf("%02x", O[Q]);
     }
 }
 
-void cobra_custom_hash(FILE *file, unsigned char *outputBuffer) {
-    uint8_t block[BLOCK_SIZE];
-    size_t bytesRead;
-
-    uint64_t H0_temp = H0;
-    uint64_t H1_temp = H1;
-    uint64_t H2_temp = H2;
-    uint64_t H3_temp = H3;
-
-    while ((bytesRead = fread(block, 1, BLOCK_SIZE, file)) > 0) {
-        process_block(block, &H0_temp, &H1_temp, &H2_temp, &H3_temp);
-    }
-
-    sprintf((char *)outputBuffer, "%016llx%016llx%016llx%016llx", H0_temp, H1_temp, H2_temp, H3_temp);
-}
-
-void print_ascii_art() {
+void U() {
     printf("\033[1;31m");
     printf("   _____      _               _    _           _     \n");
-    printf("  / ____|    | |             | |  | |         | |    \n");
+    printf("  / ____|    | |     v0.3    | |  | |         | |    \n");
     printf(" | |     ___ | |__  _ __ __ _| |__| | __ _ ___| |__  \n");
     printf(" | |    / _ \\| '_ \\| '__/ _` |  __  |/ _` / __| '_ \\ \n");
     printf(" | |___| (_) | |_) | | | (_| | |  | | (_| \\__ \\ | | |\n");
@@ -78,97 +72,97 @@ void print_ascii_art() {
     printf("\033[0m\n");
 }
 
-void print_help() {
+void V() {
     printf("\033[1;32mUsage: cobra_hash [options] <filename>\n\033[0m");
     printf("Options:\n");
     printf("  \033[1;34m-h\033[0m             Show this help message\n");
     printf("  \033[1;34m-c <hash>\033[0m      Check if the hash of the file matches the provided hash in the format:\n");
-    printf("                      %s<YOUR_HASH>_000_<YOUR_32_BYTES>%s\n", HASH_PREFIX, HASH_SUFFIX);
+    printf("                      %s<HASH>%s\n", C_PREFIX, C_SUFFIX);
     printf("\n\033[1;33mMade by JRDP Team\033[0m\n");
     printf("Contact: \033[1;34mhttps://jrdpteam.netlify.app\033[0m\n");
     printf("Main CobraHash page: \033[1;34mhttp://cobrahash.unaux.com\033[0m\n");
 }
 
-void cobra_hash(FILE *file) {
-    unsigned char custom_hash[65];
-    unsigned char extracted_bytes[32];
-
-    rewind(file);
-    cobra_custom_hash(file, custom_hash);
-
-    rewind(file);
-    extract_32_bytes(file, extracted_bytes);
-
-    printf("%s", HASH_PREFIX);
-    printf("%s_000_", custom_hash);
-    print_hex(extracted_bytes, 32);
-    printf("%s\n", HASH_SUFFIX);
-}
-
-bool check_hash(const char *input_hash, FILE *file) {
-    unsigned char custom_hash[65];
-    unsigned char extracted_bytes[32];
+bool Z(const char *input_hash, FILE *H) {
+    unsigned char X[65];
+    unsigned char Y[32];
     char computed_hash[65 + 5 + 32 * 2 + 1];
 
-    if (strncmp(input_hash, HASH_PREFIX, strlen(HASH_PREFIX)) != 0 || 
-        strncmp(&input_hash[strlen(input_hash) - strlen(HASH_SUFFIX)], HASH_SUFFIX, strlen(HASH_SUFFIX)) != 0) {
-        printf("\033[1;31mError: Hash must start with '%s' and end with '%s'\033[0m\n", HASH_PREFIX, HASH_SUFFIX);
+    if (strncmp(input_hash, C_PREFIX, strlen(C_PREFIX)) != 0 || 
+        strncmp(&input_hash[strlen(input_hash) - strlen(C_SUFFIX)], C_SUFFIX, strlen(C_SUFFIX)) != 0) {
+        printf("\033[1;31mError: Hash must start with '%s' and end with '%s'\033[0m\n", C_PREFIX, C_SUFFIX);
         return false;
     }
 
-    rewind(file);
-    cobra_custom_hash(file, custom_hash);
-    rewind(file);
-    extract_32_bytes(file, extracted_bytes);
+    const char *actual_input_hash = input_hash + strlen(C_PREFIX);
+    size_t actual_hash_length = strlen(actual_input_hash) - strlen(C_SUFFIX);
 
-    int pos = 0;
-    pos += sprintf(&computed_hash[pos], "%s", custom_hash);
-    pos += sprintf(&computed_hash[pos], "_000_");
-    for (int i = 0; i < 32; i++) {
-        pos += sprintf(&computed_hash[pos], "%02x", extracted_bytes[i]);
+    if (actual_hash_length != (64 + 5 + 64)) {
+        printf("\033[1;31mError: Invalid hash format length.\033[0m\n");
+        return false;
     }
 
-    return strcmp(input_hash, computed_hash) == 0;
+    rewind(H);
+    cobrahash(H, X, Y);
+
+    int pos = 0;
+    pos += sprintf(&computed_hash[pos], "%s", X);
+    pos += sprintf(&computed_hash[pos], "_000_");
+    for (int A = 0; A < 32; A++) {
+        pos += sprintf(&computed_hash[pos], "%02x", Y[A]);
+    }
+
+    if (strncmp(actual_input_hash, computed_hash, actual_hash_length) == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 int main(int argc, char **argv) {
-    print_ascii_art();
+    U();
+
     if (argc < 2) {
-        print_help();
+        V();
         return 1;
     }
 
     if (strcmp(argv[1], "-h") == 0) {
-        print_help();
+        V();
         return 0;
     }
 
     if (argc == 4 && strcmp(argv[1], "-c") == 0) {
         const char *provided_hash = argv[2];
-        FILE *file = fopen(argv[3], "rb");
-        if (!file) {
+        FILE *H = fopen(argv[3], "rb");
+        if (!H) {
             perror("Error opening file");
             return 1;
         }
 
-        if (check_hash(provided_hash, file)) {
+        if (Z(provided_hash, H)) {
             printf("\033[1;32mHash matches!\033[0m\n");
         } else {
             printf("\033[1;31mHash does not match!\033[0m\n");
         }
 
-        fclose(file);
+        fclose(H);
         return 0;
     }
 
-    FILE *file = fopen(argv[1], "rb");
-    if (!file) {
+    FILE *H = fopen(argv[1], "rb");
+    if (!H) {
         perror("Error opening file");
         return 1;
     }
 
-    cobra_hash(file);
-    fclose(file);
-
+    unsigned char hash[65], bytes[32];
+    cobrahash(H, hash, bytes);
+    printf("%s", C_PREFIX);
+    printf("%s_000_", hash);
+    N(bytes, 32);
+    printf("%s\n", C_SUFFIX);
+    
+    fclose(H);
     return 0;
 }
